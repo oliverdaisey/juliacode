e048afd24c7b8067af08e7afa0510e76d93c53e7
oliverdaisey <oliverjdaisey@gmail.com>
2023-11-30 15:23:08 +0000

    added type aliases

--- code/bistellarflips.jl
+++ code/bistellarflips.jl
@@ -1,17 +1,15 @@
 using Oscar
-include("cayleyembedding.jl")
 
-struct MixedCell
-   indices::Vector{NTuple{2,Int}}
-end
-
+# type aliases
+const MixedCell = Vector{NTuple{2,Int}}
+const PointConfiguration = Vector{Matrix{QQFieldElem}}
 
 
 """
 Function to compute the bistellar flip as described in Anders' paper.
 
 INPUTS:
-- `A::Vector{Matrix{QQFieldElem}}`: a vector of point configurations in the same dimension
+- `A::Vector{PointConfiguration}`: a vector of point configurations in the same dimension
 - `σ::MixedCell`: a mixed cell of the cayley embedding
 - `c::Vector{QQFieldElem}`: a circuit defining a facet F of the mixed cell cone C_σ
 - `A_mix::Vector{MixedCell}`: a vector of mixed cells sufficiently close to F
@@ -20,8 +18,11 @@ OUTPUTS:
 
 """
 function bistellar_flip(
-    A::Vector{Matrix{QQFieldElem}}, σ::MixedCell, c::Vector{QQFieldElem}, A_mix::Vector{MixedCell}
-) :: Vector{MixedCell} 
-{
+    A::Vector{PointConfiguration}, σ::MixedCell, c::Vector{QQFieldElem}, A_mix::Vector{MixedCell}
+)::Vector{MixedCell}
+    B_mix = Vector{MixedCell}()
 
-}
+    return B_mix
+    
+end
+
