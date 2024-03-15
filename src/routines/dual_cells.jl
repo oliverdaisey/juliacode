a6b24a3c43eb24adf1067078e6a35157abd04326
oliverdaisey <oliverjdaisey@gmail.com>
2024-03-15 18:36:23 +0000

    dual cell changes

--- src/routines/dual_cells.jl
+++ src/routines/dual_cells.jl
@@ -7,11 +7,18 @@ Create a vector of all the maximal dual cells from a support (of exponent vector
 
 TODO: Return *all* dual cells, not just the maximal ones.
 """
-function dual_cells(S::Support{<:DualCell}, c::Vector{Oscar.TropicalSemiringElem})
+function dual_cells(S::Support{<:DualType}, c::Vector{Oscar.TropicalSemiringElem})
 
 
     dualSubdivision = subdivision_of_points_workaround(S.points, c)
     polyhedralComplex = polyhedral_complex(Oscar.pm_object(dualSubdivision).POLYHEDRAL_COMPLEX)
+
+    dualCells = []
+    for i in 1:dim(polyhedralComplex)
+        for p in polyhedra_of_dim(polyhedralComplex, i)
+            Vector{Int}.(collect(vertices(p)))
+        end
+    end
 
     
 
